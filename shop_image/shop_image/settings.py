6ebe346a9d171493fc23ced7baa118cdc4a361a3
Ajibson <azeezajibson123@gmail.com>
2021-09-14 20:36:43 +0100

    static files 😎

--- shop_image/shop_image/settings.py
+++ shop_image/shop_image/settings.py
@@ -125,9 +125,10 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = 'media/'
-STATIC_ROOT = BASE_DIR / 'static'
+# STATIC_ROOT = BASE_DIR / 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
