9c0e1406184fc468a408ed765037e7074c6b0f31
Ajibson <azeezajibson123@gmail.com>
2021-09-14 22:46:38 +0100

    image processing library added

--- images/models.py
+++ images/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from users.models import User
 from django.utils import timezone
+from django_resized import ResizedImageField
 
 
 class Category(models.Model):
@@ -16,14 +17,16 @@ class Category(models.Model):
 
 class Image(models.Model):
     user = models.ForeignKey("users.User", on_delete=models.CASCADE)
-    image = models.ImageField(upload_to="images")
+    # image = models.ImageField(upload_to="images", null=True, blank=True)
     image_name = models.CharField(max_length=250)
+    image = ResizedImageField(
+        size=[350, 350], upload_to="images", blank=True, null=True, force_format='PNG')
     image_category = models.ForeignKey(
         Category, on_delete=models.CASCADE, blank=True, null=True)
     price = models.IntegerField(default=0)
-    amount_made = models.IntegerField(default=0)
-    number_of_download = models.IntegerField(default=0)
-    description = models.TextField(blank=True, null=True)
+    amount_made = models.IntegerField(default=0, blank=True)
+    number_of_download = models.IntegerField(default=0, blank=True)
+    # description = models.TextField(blank=True, null=True)
     date_created = models.DateTimeField(default=timezone.now, blank=True)
 
     def __str__(self):
