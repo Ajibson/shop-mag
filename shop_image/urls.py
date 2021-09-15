451550918ee03b0f2ee23c2c4fbcbe79428ad16f
Ajibson <azeezajibson123@gmail.com>
2021-09-15 07:19:04 +0100

    collect static command ran after debug set false

--- shop_image/urls.py
+++ shop_image/urls.py
@@ -12,4 +12,7 @@ urlpatterns = [
     path("images/", include("images.urls")),
     path("payments/", include("payments.urls")),
     path("", index, name='index')
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+
+if settings.DEBUG:
+    urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
