4a711aef4399e3f04966a8a1c502de7bf1fd6b9d
Ajibson <azeezajibson123@gmail.com>
2021-09-15 07:24:15 +0100

    cloudinary storage😎

--- shop_image/settings.py
+++ shop_image/settings.py
@@ -26,8 +26,8 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
     'cloudinary_storage',
+    'django.contrib.staticfiles',
     'cloudinary',
     # local apps
     'users',
